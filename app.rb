8b6d09d883e03e5a96134c8066e1f133d767e94f
Kay Mok <kaymok3@gmail.com>
2018-04-13 15:49:00 -0400

    Add individual staff pages

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'team'
 
 get '/' do
-  erb :index
+  erb :index, :layout => :home_layout
 end
 
 get '/about' do
@@ -18,13 +19,17 @@ get '/team' do
 end
 
 get '/team/:name' do
-  if params[:name] == "andy"
-    erb :andy
-  elsif params[:name] == "dwight"
-    erb :dwight
+  if params[:name] == "dwight"
+    @dwight = Staff.new('Dwight Schrute', '/images/staff/dwight.jpg', 'Assistant (to the) Regional Manager', 'Hard-working, Alpha male. Jackhammer. Merciless. Insatiable.')
+    erb :dwight, :layout => :team_layout
   elsif params[:name] == "jim"
-    erb :jim
+    @jim = Staff.new('Jim Halpert', '/images/staff/jim.jpg', 'Salesman', 'Office Prankster')
+    erb :jim, :layout => :team_layout
+  elsif params[:name] == "andy"
+    @andy = Staff.new('Andy Bernard', '/images/staff/andy.jpg', 'Salesman', 'The Nard Dog. Proud Cornell Graduate.')
+    erb :andy, :layout => :team_layout
   else
-    erb :michael
+    @michael = Staff.new('Michael Scott', '/images/staff/michael.jpg', 'Regional Manager', "World's Best Boss")
+    erb :michael, :layout => :team_layout
   end
 end
