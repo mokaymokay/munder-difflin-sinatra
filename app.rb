85115f24898c821434bfac2c576dce043de256f7
Kay Mok <kaymok3@gmail.com>
2018-04-13 13:50:13 -0400

    Add some routes

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get "/" do
+get '/' do
   redirect '/index.html'
+end
+
+get '/about-us' do
+  redirect '/index.html#about-us'
+end
+
+get '/contact' do
+  redirect '/index.html'
+end
+
+get '/staff' do
+  redirect '/index.html#team'
 end
