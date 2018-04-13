39080ef127609b7288ca993f1f8beefed132679c
Kay Mok <kaymok3@gmail.com>
2018-04-13 16:46:04 -0400

    Dynamically generate jokes from Staff class

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org' do
   gem 'sinatra'
+  gem 'chuck_norris'
 end
--- team.rb
+++ team.rb
@@ -1,11 +1,14 @@
-class Staff
-  attr_reader :name, :photo, :position, :description
+require 'chuck_norris'
 
-  def initialize(name, photo, position, description)
+class Staff
+  attr_reader :name, :photo, :position, :description, :joke
+
+  def initialize(name, photo, position, description, joke = ChuckNorris::JokeFinder.find_joke('random?limitTo=[nerdy]').joke)
     @name = name
     @photo = photo
     @position = position
     @description = description
+    @joke = joke
   end
 
 end
