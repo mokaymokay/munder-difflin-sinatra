e2500890fb00d2ee60e00a112e4b4c86be225f8e
Kay Mok <kaymok3@gmail.com>
2018-04-16 10:43:14 -0400

    Refactor to use only one instance variable and erb file for all staff members

--- app.rb
+++ app.rb
@@ -19,19 +19,20 @@ get '/team' do
 end
 
 get '/team/:name' do
-  if params[:name] == "dwight"
-    @dwight = Staff.new('Dwight Schrute', '/images/staff/dwight.jpg', 'Assistant (to the) Regional Manager', 'Hard-working, Alpha male. Jackhammer. Merciless. Insatiable.')
-    erb :dwight, :layout => :team_layout
-  elsif params[:name] == "jim"
-    @jim = Staff.new('Jim Halpert', '/images/staff/jim.jpg', 'Salesman', 'Office Prankster')
-    erb :jim, :layout => :team_layout
-  elsif params[:name] == "andy"
-    @andy = Staff.new('Andy Bernard', '/images/staff/andy.jpg', 'Salesman', 'Nard Dog. Proud Cornell Graduate.')
-    erb :andy, :layout => :team_layout
-  elsif params[:name] == "michael"
-    @michael = Staff.new('Michael Scott', '/images/staff/michael.jpg', 'Regional Manager', "World's Best Boss")
-    erb :michael, :layout => :team_layout
-  else
-    erb :group, :layout => :team_layout
-  end
+  case params[:name]
+    when "dwight"
+      @staff = Staff.new('Dwight Schrute', '/images/staff/dwight.jpg', 'Assistant (to the) Regional Manager', 'Hard-working, Alpha male. Jackhammer. Merciless. Insatiable.')
+      erb :staff, :layout => :team_layout
+    when "jim"
+      @staff = Staff.new('Jim Halpert', '/images/staff/jim.jpg', 'Salesman', 'Office Prankster')
+      erb :staff, :layout => :team_layout
+    when "andy"
+      @staff = Staff.new('Andy Bernard', '/images/staff/andy.jpg', 'Salesman', 'Nard Dog. Proud Cornell Graduate.')
+      erb :staff, :layout => :team_layout
+    when "michael"
+      @staff = Staff.new('Michael Scott', '/images/staff/michael.jpg', 'Regional Manager', "World's Best Boss")
+      erb :staff, :layout => :team_layout
+    else
+      raise "That employee doesn't exist"
+    end
 end
